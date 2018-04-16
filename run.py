d82754f95569020435c73b3b15062fc52fc3460e
Soichi Hayashi <hayashis@iu.edu>
2018-04-16 16:42:55 +0000

    fixed incorrect invocation of t2 check during t1 test

--- run.py
+++ run.py
@@ -23,7 +23,6 @@ results = {"errors": [], "warnings": []}
 
 directions = None
 
-
 def check_affine(affine):
     if affine[0][0] != 1: results['warnings'].append("transform matrix 0.1 is not 1")
     if affine[0][1] != 0: results['warnings'].append("transform matrix 0.2 is not 0")
@@ -35,60 +34,38 @@ def check_affine(affine):
     if affine[2][1] != 0: results['warnings'].append("transform matrix 2.1 is not 0")
     if affine[2][2] != 1: results['warnings'].append("transform  matrix 2.2 is not 1")
 
-
 # TODO - I am not sure what I need to do differently between t1 and t2
-def validate_t1t2(path):
+def validate_anat(path):
 
     try:
-        print('checking t1')
-        img = nibabel.load(config['t1'])
-        results['t1_headers'] = str(img.header)
-        results['t1_base_affine'] = str(img.header.get_base_affine())
+        print('checking anatomy')
+        img = nibabel.load(path)
+        results['headers'] = str(img.header)
+        results['base_affine'] = str(img.header.get_base_affine())
 
         # check dimensions
         dims = img.header['dim'][0]
         if dims != 3:
-            results['errors'].append("T1 should be 3D but has " + str(dims))
+            results['errors'].append("input should be 3D but has " + str(dims))
 
         check_affine(img.header.get_base_affine())
 
     except Exception as e:
         results['errors'].append("nibabel failed on t1. error code: " + str(e))
 
-    try:
-        print('checking t2')
-        img = nibabel.load(config['t2'])
-        results['t2_headers'] = str(img.header)
-        results['t2_base_affine'] = str(img.header.get_base_affine())
-
-        # check dimensions
-        dims = img.header['dim'][0]
-        if dims != 3:
-            results['errors'].append("T2 should be 3D but has " + str(dims))
-
-        check_affine(img.header.get_base_affine())
-
-    except Exception as e:
-        results['errors'].append("nibabel failed on t2. error code: " + str(e))
-
-
 if config.has_key('t1'):
-    validate_t1t2(config['t1'])
+    validate_anat(config['t1'])
 
     # TODO - normalize (for now, let's just symlink)
     # TODO - if it's not .gz'ed, I should?
     os.symlink(config['t1'], "t1.nii.gz")
 
 if config.has_key('t2'):
-    validate_t1t2(config['t2'])
+    validate_anat(config['t2'])
 
     # TODO - normalize (for now, let's just symlink)
     # TODO - if it's not .gz'ed, I should?
     os.symlink(config['t2'], "t2.nii.gz")
-
-# deprecated
-with open("products.json", "w") as fp:
-    json.dump([results], fp)
 
 with open("product.json", "w") as fp:
     json.dump(results, fp)
